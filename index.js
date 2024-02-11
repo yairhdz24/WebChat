2928b93cdafe30161104b0f6666da25053e4ba97
yairhdz24 <yairhdz107@gmail.com>
2024-02-10 21:55:42 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var socket = require("socket.io");
+
+// Create a new express application
+var app = express();
+
+var server = app.listen(4000, function () {
+  console.log("Server is running on port 4000");
+});
+
+//static files
+app.use(express.static("public"));
+
+// Socket settings
+var io = socket(server);
+
+io.on("connection", function (socket) {
+  console.log("A user connected: " + socket.id);
+
+  socket.on("chat message", function (data) {
+    io.sockets.emit("chat message", data);
+  
+    socket.on("typing", function (data) {
+      socket.broadcast.emit("typing", data);
+    });
+
+    socket.on("disconnect", function () {
+      console.log("user disconnected: " + socket.id);
+    });
+  });
+});
---
+++ public/chart.js
@@ -0,0 +1,45 @@
+var socket = io.connect("http://localhost:4000");
+
+// Query DOM
+var message = document.getElementById("message"),
+  handle = document.getElementById("handle"),
+  btn = document.getElementById("send"),
+  output = document.getElementById("output"),
+  feedback = document.getElementById("feedback");
+
+message.addEventListener("keypress", function (event) {
+  if (event.key === 'Enter') {
+    event.preventDefault(); 
+    document.getElementById("send").click(); // Trigger para que le de click al enviar
+  } else {
+    socket.emit("typing", handle.value);
+  }
+});
+
+// Emit events
+btn.addEventListener("click", function () {
+  socket.emit("chat message", {
+    message: message.value,
+    handle: handle.value,
+  });
+  message.value = "";
+});
+
+// Listen for events
+socket.on("chat message", function (data) {
+  feedback.innerHTML = "";
+  output.innerHTML +=
+    "<p><strong>" + data.handle + ": </strong>" + data.message + "</p>";
+});
+
+socket.on("typing", function (data) {
+  feedback.innerHTML = "<p><em>" + data + " Escribiendo...</em></p>";
+  clearTimeout(typingTimeout);
+  var typingTimeout = setTimeout(function () {
+    feedback.innerHTML = "";
+  }, 4000); // Elimina el mensaje de "Escribiendo..." despues de 4 segundos
+});
+
+socket.on("disconnect", function () {
+  feedback.innerHTML = "<p><em>" + handle.value + " se desconecto</em></p>";
+});
