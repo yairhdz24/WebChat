dae5c3f2b15f82ecd3d4761ac5a6e19b6b6a24fa
yairhdz24 <yairhdz107@gmail.com>
2024-02-12 21:07:50 -0600

    Add socket settings and configure server port

--- index.js
+++ index.js
@@ -6,25 +6,30 @@ var socketIo = require("socket.io");
 var app = express();
 var server = http.createServer(app);
 
+var port = process.env.PORT || 3000;
+server.listen(port, function () {
+    console.log("Servidor escuchando en el puerto " + port);
+});
+
 // Configurar el servidor de sockets
 var io = socketIo(server);
 
 //static files
 app.use(express.static("public"));
 
-
+// Socket settings
 io.on("connection", function (socket) {
   console.log("A user connected: " + socket.id);
 
   socket.on("chat message", function (data) {
     io.sockets.emit("chat message", data);
-  
-    socket.on("typing", function (data) {
-      socket.broadcast.emit("typing", data);
-    });
+  });
 
-    socket.on("disconnect", function () {
-      console.log("user disconnected: " + socket.id);
-    });
+  socket.on("typing", function (data) {
+    socket.broadcast.emit("typing", data);
+  });
+
+  socket.on("disconnect", function () {
+    console.log("user disconnected: " + socket.id);
   });
 });
